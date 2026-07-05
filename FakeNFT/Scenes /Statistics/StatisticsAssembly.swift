2f950d3bac41001f0f4357094acbabafcc6ed542
Chel Pchelov <danmigunov@gmail.com>
2026-07-05 03:38:34 +0300

    statistics section added

---
+++ FakeNFT/Scenes /Statistics/StatisticsAssembly.swift
@@ -0,0 +1,23 @@
+//
+//  StatisticsAssembly.swift
+//  FakeNFT
+//
+import UIKit
+
+class StatisticsAssembly {
+    private let servicesAssembler: ServicesAssembly
+    
+    init(servicesAssembler: ServicesAssembly) {
+        self.servicesAssembler = servicesAssembler
+    }
+    
+    func build() -> UIViewController {
+        let presenter = StatisticsPresenter(userService: servicesAssembler.userService)
+        
+        let viewController = StatisticsViewController(presenter: presenter)
+        
+        presenter.view = viewController
+        
+        return viewController
+    }
+}
