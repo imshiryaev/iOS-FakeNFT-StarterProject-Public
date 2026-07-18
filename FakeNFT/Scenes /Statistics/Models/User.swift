68c1bf7cc4625a01341aa2e07bd1d05347a8faf7
Chel Pchelov <danmigunov@gmail.com>
2026-07-19 01:38:18 +0300

    model description added

--- FakeNFT/Scenes /Statistics/Models/User.swift
+++ FakeNFT/Scenes /Statistics/Models/User.swift
@@ -5,11 +5,24 @@
 import Foundation
 
 struct User: Codable {
+    /// Имя пользователя.
     let name: String
+
+    /// URL изображения профиля.
     let avatar: URL
+
+    /// Описание профиля пользователя.
     let description: String?
+
+    /// URL сайта пользователя.
     let website: URL?
+
+    /// Идентификаторы NFT, принадлежащих пользователю.
     let nfts: [UUID]
+
+    /// Позиция пользователя в рейтинге.
     let rating: Int
+
+    /// Уникальный идентификатор пользователя.
     let id: UUID
 }
