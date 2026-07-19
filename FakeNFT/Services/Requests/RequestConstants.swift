3e20cce5223ce73598013b8b97414f2e65d82963
Chel Pchelov <danmigunov@gmail.com>
2026-07-19 23:33:16 +0300

    xcconfig stub file added

--- FakeNFT/Services/Requests/RequestConstants.swift
+++ FakeNFT/Services/Requests/RequestConstants.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 enum RequestConstants {
+#warning("Enter your token at ServerVariablesPublic.xcconfig before building")
     static let baseURL = Bundle.main.object(forInfoDictionaryKey: "BASE_URL") as? String ?? ""
     static let token = Bundle.main.object(forInfoDictionaryKey: "TOKEN") as? String ?? ""
 }
